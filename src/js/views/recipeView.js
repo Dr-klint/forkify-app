fde282730a84422bd0d606f890140f323abc19c1
Dr-klint <okechukwuclinton005@gmail.com>
2022-09-26 22:52:24 +0100

    resolved

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -3,8 +3,8 @@ import View from './View.js';
 // import icons from '../img/icons.svg'; //parcel 1 method
 import icons from 'url:../../img/icons.svg'; //parcel 2 method
 
-import { Fraction } from 'fractional'; //when using import from npm, there isnt a need to direct any path, just state the name of the npm package
-console.log(Fraction);
+//import { Fraction } from 'fractional'; when using import from npm, there isnt a need to direct any path, just state the name of the npm package
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -137,8 +137,7 @@ class RecipeView extends View {
     <use href="${icons}#icon-check"></use>
   </svg>
   <div class="recipe__quantity">${
-    // ing.quantity ? ing.quantity : ''
-    ing.quantity ? new Fraction(ing.quantity).toString() : ''
+    ing.quantity ? fracty(ing.quantity).toString() : ''
   }</div>
   <div class="recipe__description">
     <span class="recipe__unit">${ing.unit}</span>
